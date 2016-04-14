a1add6f25c038267a3b3c6c29c9118873c02aa33
Grzegorz Lyczba <grzegorz.lyczba@gmail.com>
2016-04-14 15:05:32 +0200

    canvas test

---
+++ canvas/canvas.js
@@ -0,0 +1,23 @@
+function addCanvas () {
+	var canvas = document.createElement("canvas");
+	var context = canvas.getContext("2d");
+	var width = 125;  // Triangle Width
+	var height = 105; // Triangle Height
+	var padding = 20;
+
+	// Draw a path
+	context.beginPath();
+	context.moveTo(padding + width/2, padding);        // Top Corner
+	context.lineTo(padding + width, height + padding); // Bottom Right
+	context.lineTo(padding, height + padding);         // Bottom Left
+	context.closePath();
+
+	// Fill the path
+	context.fillStyle = "#ffc821";
+	context.fill();
+
+	var currentDiv = document.getElementById("div1");
+	document.body.insertBefore(canvas, currentDiv);
+}
+
+window.onload = addCanvas;
