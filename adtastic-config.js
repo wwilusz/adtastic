5a797eb6204df7aaabbabd6be8f89967428b06df
Grzegorz Lyczba <soymo@users.noreply.github.com>
2016-07-01 10:52:29 +0200

    Create adtastic-config.js

---
+++ adtastic-config.js
@@ -0,0 +1,34 @@
+__aaWrapperCfg({
+  customerId: 'adtastic',
+  collectorUrl: 'http://ci-aa-collector.mezzobit.com/',
+
+  replacement_groups: [{
+    url_pattern: /.*/,
+    slots: {
+      'div-gpt-ad-1467286751287-0': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+      'div-gpt-ad-1467286751287-1': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+      'div-gpt-ad-1467286751287-2': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+      'div-gpt-ad-1467286751287-3': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+    }
+  }]
+});
