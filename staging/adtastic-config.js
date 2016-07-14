1494726eb03f741d7c80c6e92de94c9443e570ef
Grzegorz Lyczba <grzegorz.lyczba@gmail.com>
2016-07-14 14:49:05 +0200

    staging

---
+++ staging/adtastic-config.js
@@ -0,0 +1,34 @@
+__aaWrapperCfg({
+  customerId: 'adtastic',
+  collectorUrl: 'http://staging-data.aaplat.net/',
+
+  replacement_groups: [{
+    url_pattern: /.*/,
+    slots: {
+      'div-gpt-ad-1467286751287-0': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+      'div-gpt-ad-1467286751287-1': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+      'div-gpt-ad-1467286751287-2': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+      'div-gpt-ad-1467286751287-3': {
+        provider: 1,
+        options: {
+          zoneId: 43737
+        }
+      },
+    }
+  }]
+});
